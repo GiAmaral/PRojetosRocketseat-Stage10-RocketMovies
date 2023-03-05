045587d6a3170e27f75c0e8788b0378802fbe4d8
gisele <gisele.gfa89@gmail.com>
2023-03-05 17:16:34 -0300

    baseURL changed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://rocketmovies-server-gg54.onrender.com",
 });
 
 api.interceptors.request.use(
